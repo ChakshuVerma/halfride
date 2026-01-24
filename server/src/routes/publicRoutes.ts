dd62e0c1abd3214dcd417037bd603f8dea063e64
ChakshuVerma <vermachakshu@gmail.com>
2026-01-24 18:40:01 +0530

    Removed junk code

--- server/src/routes/publicRoutes.ts
+++ server/src/routes/publicRoutes.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
 import { optionalAuth } from '../middleware/auth';
-import { publicData, publicFlightStatus } from '../controllers/publicController';
+import { publicData } from '../controllers/publicController';
 
 export const publicRouter = Router();
 
 publicRouter.get('/public/data', optionalAuth, publicData);
-publicRouter.get('/public/flight-status', publicFlightStatus);
 
